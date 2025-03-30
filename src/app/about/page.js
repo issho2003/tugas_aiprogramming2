6844abba0e4b840c35fdc33e8214f4f8a9da4894
issho2003 <issho2003@gmail.com>
2025-03-30 18:26:45 +0700

    add about page

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl p-2 space-y-4'>
+      <h1 className='text-3xl font-medium text-amber-500'>About</h1>
+    </div>
+  )
+}
